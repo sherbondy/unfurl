dd5db234325faf9e5986fea531a1b33ed96b6be4
Ethan Sherbondy <sherbondye@gmail.com>
2016-04-10 01:25:13 -0400

    initial working commit.

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject unfurle-circle "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.4.0"]
+                 [org.clojure/clojurescript "1.8.34"]]
+
+  :plugins [[lein-cljsbuild "1.1.3"]]
+  :hooks [leiningen.cljsbuild]
+
+  :cljsbuild
+  {:builds [{:source-paths ["src"]
+             :compiler
+             {:output-to "js/main.js"
+              :output-dir "out"
+              :main "unfurl_circle.core"
+              :optimizations :none
+              :pretty-print true}}]})
